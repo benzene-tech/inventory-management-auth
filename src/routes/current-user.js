e495d7464855e2d406b881fd8d7106155b41b438
Sanjaykumar <sanjaykumar1481999@gmail.com>
2021-06-19 18:25:55 +0530

    [sanjay] updated current user route

--- src/routes/current-user.js
+++ src/routes/current-user.js
@@ -1,15 +1,25 @@
 const { checkAuth } = require('@benzene-tech/inventory-management-core');
 const { Router } = require('express');
 const jwt = require('jsonwebtoken');
+const Auth = require('../models/auth');
 
 const router = Router();
 
-router.get('/api/auth/current-user', checkAuth, (req, res) => {
+router.get('/api/auth/current-user', checkAuth, async (req, res) => {
   const bearerToken = req.headers.authorization.split(' ')[1];
 
   const { id, username } = jwt.decode(bearerToken, process.env.JWT_SECRET);
 
-  res.status(200).send({ user: { _id: id, username } });
+  const auth = await Auth.findOne({ username });
+
+  res.status(200).send({
+    user: {
+      _id: id,
+      username,
+      storeId: auth.storeId,
+      userType: auth.userType,
+    },
+  });
 });
 
 module.exports = router;
